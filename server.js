5fd9159aaa5e38c914e71d5b3e7e20ab447baea5
Wedant <70766002+Wedant@users.noreply.github.com>
2020-09-10 16:14:04 +0100

    add first GET route

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.listen(8000,function(){console.log('Server is running')});
+// app.get('/',(req,res)=>{res.send('Server is running. Nodemon test.')});
 
-app.get('/',(req,res)=>{
-    res.send('Server is running');
-});
+const mockUserData=[
+    {name:'Jack'},
+    {name:'Jill'}
+]
+app.get('/user', function(req,res){
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        user: mockUserData
+    })
+})
+
+app.listen(8000,function(){console.log('Server is running')});
